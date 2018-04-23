561fd952be6c0d377bd7f3e8e6aa866859b1d269
wyinnn <wyinkevin@gmail.com>
2018-04-23 19:57:06 -0400

    scene3 build

---
+++ Script/level3-elevator/FoodHolder3.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodHolder3 : MonoBehaviour {
+
+    public float cdTime;
+    public float countDown;
+
+
+    void Update()
+    {
+        countDown -= Time.deltaTime;
+
+
+
+        if (countDown < 0)
+        {
+            int childNum;
+            countDown = cdTime;
+            childNum = Random.Range(0, 4);
+
+            GameObject randomChild = transform.GetChild(childNum).gameObject;
+            GameObject newChild = Instantiate(randomChild, new Vector3(0f, -5.64f, -0.5f), new Quaternion(0f, 0f, 0f, 0f)) as GameObject;
+            newChild.SetActive(true);
+        }
+    }
+}
---
+++ Script/level3-elevator/rightwall.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rightwall : MonoBehaviour {
+    private Rigidbody itself;
+    public bool buff_frozen;
+    public float buff_begin_time;
+    public float buff_exist_time;
+    
+    void Start () {
+        itself = gameObject.GetComponent<Rigidbody>();
+	}
+    void testbuff()
+    {
+        if (buff_begin_time != 0)
+        {
+            if (Time.time - buff_begin_time >= buff_exist_time)
+            {
+                buff_frozen = false;
+                buff_begin_time = 0;
+            }
+            else
+                buff_frozen = true;
+        }
+    }
+    void Buff_Time(float buff_begin)//
+    {
+        buff_begin_time = buff_begin;
+
+
+    }
+    // Update is called once per frame
+    void Update () {
+        testbuff();
+        float v_dir = Input.GetAxis("J2-Vertical");
+        if (buff_frozen)
+        {
+            v_dir = 0;
+        }
+        itself.velocity = new Vector3(0,  v_dir, 0);
+    }
+}
