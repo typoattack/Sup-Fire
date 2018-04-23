2d75bba3b238fa9d935aa7734cad6e20d5937683
Levin2504 <Levin2504@gmail.com>
2018-04-23 15:11:07 -0400

    fixed P2 turret rotation issue and modified snow material

--- Script/level1-origin/ControllerP2.cs
+++ Script/level1-origin/ControllerP2.cs
@@ -196,7 +196,7 @@ public class ControllerP2 : MonoBehaviour
     void Start()
     {
         rigid = this.GetComponent<Rigidbody>();
-        transform.GetChild(1).transform.Rotate(0f, 90f, 0f);
+        //transform.GetChild(1).transform.Rotate(0f, 90f, 0f);
     }
 
 
