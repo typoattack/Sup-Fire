b9c2036a59c6c6e8735e9b260982ed837a3af94d
wyinnn <wyinkevin@gmail.com>
2018-04-28 00:47:08 -0400

    scene5

---
+++ Script/level5-planet/gravity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravity : MonoBehaviour {
+    private Rigidbody itself;
+	// Use this for initialization
+	void Start () {
+        itself=GetComponent<Rigidbody>();
+       // itself.AddForce(10, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       Vector3 dir = 10*(new Vector3(0,0,0)-transform.position).normalized;
+        itself.AddForce(dir);
+    }
+}
