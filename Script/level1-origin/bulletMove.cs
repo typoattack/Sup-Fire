42943b07d1e58ef2c85ab6a40b828a7cb2add86d
typoattack <typoattack@gmail.com>
2018-04-28 21:25:23 -0400

    Added submarine "torpedoes"

--- Script/level1-origin/bulletMove.cs
+++ Script/level1-origin/bulletMove.cs
@@ -12,19 +12,21 @@ public class bulletMove : MonoBehaviour {
     public bool isFrozen;//
 
     GameObject[] sparks;
+    GameObject waterSplatter;
     GameObject[] explosion;
     GameObject[] delay;
     GameObject[] hit;
 
     public AudioSource expSound;
     public AudioSource hitSound;
+    public AudioSource waterSound;
 
     private bool damagded = false;
 
     private void Awake()
     {
         sparks = GameObject.FindGameObjectsWithTag("sparks");
-
+        waterSplatter = GameObject.Find("FX_WaterSplatter");
     }
     void Start () {
         explosion = GameObject.FindGameObjectsWithTag("explosion");
@@ -104,7 +106,51 @@ public class bulletMove : MonoBehaviour {
 
             }
         }
+        else if (other.tag == "water")
+        {
+            waterSound.pitch = 0.1f * 1.05946f * Random.Range(8, 15);
+            waterSound.Play();
+            GameObject newSplatters = Instantiate(waterSplatter, transform.position, new Quaternion()) as GameObject;
+            if (isBig)
+            {
+                ParticleSystem SplattersParticle = newSplatters.GetComponent<ParticleSystem>();
+                var main = SplattersParticle.main;
+                main.startSize = 0.4f;
+                main.startSpeed = 5f;
+            }
 
-        
+            if (comeFrom.activeSelf)
+            {
+                comeFrom.SendMessage("SetAmmo", isMulti ? 0.5f : 1f);
+            }
+
+            Destroy(gameObject);
+            Destroy(newSplatters, 1.5f);
+        }
+        else if (other.tag == "sub")
+        {
+            hitSound.pitch = 0.1f * 1.05946f * Random.Range(8, 15);
+            //0.8-1.5 as normal, 0.5-0.8 as big, need more modification
+            hitSound.Play();
+            GameObject newSparks = Instantiate(sparks[0], transform.position, transform.rotation) as GameObject;
+            if (isBig)
+            {
+                newSparks.transform.localScale = new Vector3(2f, 2f, 2f);
+                CameraShaker.Instance.ShakeOnce(2f, 4f, 0f, 3f);
+            }
+            else
+            {
+                CameraShaker.Instance.ShakeOnce(1.25f, 4f, 0f, 1.0f);
+            }
+            if (comeFrom.activeSelf)
+            {
+                comeFrom.SendMessage("SetAmmo", isMulti ? 0.5f : 1f);
+            }
+            Destroy(gameObject);
+            Destroy(newSparks, 0.5f);
+            SubMover.torpedohitcount++;
+        }
+
+
     }
 }
