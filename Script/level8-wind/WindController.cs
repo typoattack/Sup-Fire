1d8208ca97c6fb614d35d8a08b4ce78b0f580909
typoattack <typoattack@gmail.com>
2018-05-07 19:56:55 -0400

    Implemented wind mechanics
    Wind first blows 5s after level start, then changes every 10s
    Wind intensity is fixed, either 0.5 left, 0.5 right, or no wind
    No indication currently of when wind changes

---
+++ Script/level8-wind/WindController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindController : MonoBehaviour {
+
+    [HideInInspector] public static float wind;
+    private int randomNumber;
+    
+	void Start () {
+        wind = 0;
+        randomNumber = 0;
+        InvokeRepeating("BlowWind", 5.0f, 10.0f);
+    }
+
+    void BlowWind()
+    {
+        randomNumber = Random.Range(-4, 4);
+        if (randomNumber < -1) wind = -0.5f;
+        else if (randomNumber >= -1 && randomNumber <= -1) wind = 0f;
+        else wind = 0.5f;
+
+    }
+}
