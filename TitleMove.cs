669493a0ea9be269cd3017b61e569d6679af70f4
Levin2504 <Levin2504@gmail.com>
2018-05-08 17:01:34 -0400

    title animation

---
+++ TitleMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMove : MonoBehaviour {
+
+    public float StayTime;
+    public Animator TitleAnim;
+
+    bool Played = false;
+
+    private void FixedUpdate()
+    {
+        if (!Played && Time.timeSinceLevelLoad > StayTime)
+        {
+            
+            transform.Translate(0f, 6f, 0f);
+            TitleAnim.Play("TitleOut Animation");
+            Played = !Played;
+        }
+    }
+
+}
