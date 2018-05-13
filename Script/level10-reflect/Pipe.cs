7daf1098d18a9f1a9e6f27a3f556cf43b9c0bc1b
wyinnn <wyinkevin@gmail.com>
2018-05-13 03:16:40 -0400

    foodholder down
    foodholder finished
    reflect finished
    UI in process

---
+++ Script/level10-reflect/Pipe.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour {
+    public float cd;
+    private float temp;
+    private int lastkid;
+    public Material normal;
+    public Material food;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Time.time - temp >= cd)
+        {
+
+            int childNum;
+            childNum = Random.Range(0, 14);
+ 
+            gameObject.transform.GetChild(childNum).GetComponent<MeshRenderer>().material = food;
+            gameObject.transform.GetChild(childNum).gameObject.SendMessage("befoodhoodler", true);
+            gameObject.transform.GetChild(lastkid).GetComponent<MeshRenderer>().material = normal;
+            gameObject.transform.GetChild(lastkid).gameObject.SendMessage("befoodhoodler", false);
+
+            lastkid = childNum;
+           temp = Time.time;
+
+        }
+    }
+}
---
+++ Script/level10-reflect/brick.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brick : MonoBehaviour {
+  
+    private Vector3 place;
+    public bool foodornot;
+    private GameObject target;
+ 
+	// Use this for initialization
+	void Start () {
+        place = transform.position + new Vector3(0, 1, 0);
+	}
+
+    void befoodhoodler(bool set)
+    {
+        foodornot = set;
+       
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+
+        if (foodornot == true)
+        {
+
+
+                int childNum;
+
+                childNum = Random.Range(0, 4);
+                GameObject randomChild = transform.GetChild(childNum).gameObject;
+                GameObject newChild = Instantiate(randomChild, place, new Quaternion(0f, 0f, 0f, 0f)) as GameObject;
+            bullet_move_l10 bullet = other.gameObject.GetComponent<bullet_move_l10>();
+            target = bullet.comeFrom;
+
+            if(childNum==0)
+            target.SendMessage("SetBig");
+            else if(childNum==1)
+                target.SendMessage("SetNulti");
+            else if (childNum == 2)
+                target.SendMessage("SetMissile");
+            else if (childNum == 3)
+                target.SendMessage("SetFrozen");
+
+            newChild.SetActive(true);
+                foodornot = false;
+ 
+        }
+    }
+
+  
+
+}
