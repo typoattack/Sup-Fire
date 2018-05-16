a60773910fde6fc2ccb5b8cdbdbb6b5dfc095102
Levin2504 <Levin2504@gmail.com>
2018-05-15 22:23:05 -0400

    clock design

--- rotateWall.cs
+++ rotateWall.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class rotateWall : MonoBehaviour {
+
     public float ang;
-	// Use this for initialization
+    public float angSpeed;
 	void Start () {
         ang = 90;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        ang += 10*Time.deltaTime;
+        ang += angSpeed * Time.deltaTime;
         transform.rotation = Quaternion.Euler(0, 0, ang);
 	}
 }
