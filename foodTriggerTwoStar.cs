a5e706ea47f7f100fb415d52e102b95523cba99d
Levin2504 <Levin2504@gmail.com>
2018-07-22 18:09:27 -0400

    Revert "Revert "8.3 two star with orbit""
    This reverts commit 6fadc30651b857025cff217001678b7f3e8e99b0.

---
+++ foodTriggerTwoStar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foodTriggerTwoStar : MonoBehaviour
+{
+
+    public float cdTime;
+    public float countDown;
+    public Vector3 spawnPoint;
+
+
+    void Update()
+    {
+        countDown -= Time.deltaTime;
+
+
+
+        if (countDown < 0)
+        {
+            int childNum;
+            countDown = cdTime;
+            childNum = Random.Range(0, 3);
+
+            GameObject randomChild = transform.GetChild(childNum).gameObject;
+            GameObject newChild = Instantiate(randomChild, spawnPoint, new Quaternion(0f, 0f, 0f, 0f)) as GameObject;
+            newChild.SetActive(true);
+            newChild.GetComponent<Rigidbody>().velocity = new Vector3(0f, -2f, 0f);
+        }
+    }
+}
