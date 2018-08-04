11e236d14319ca907485dd9128aa77999a63e9da
typoattack <typoattack@gmail.com>
2018-08-03 20:59:04 -0400

    Stormy level v0

---
+++ Script/World 5 -- Water/5-4 -- Foul Winds and Stormy Seas/SeaController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeaController : MonoBehaviour {
+
+    public GameObject SeaLR;
+    public GameObject SeaRL;
+
+    [HideInInspector] public static float dir;
+    private int randomNumber;
+    public float elapsedTime;
+
+    void Start()
+    {
+        dir = 0;
+        randomNumber = 0;
+        //Invoke("WaveSea", 0.0f);
+        SeaLR.gameObject.SetActive(true);
+        SeaRL.gameObject.SetActive(true);
+        InvokeRepeating("WaveSea", 0.0f, 4 * Mathf.PI);
+    }
+
+    private void Update()
+    {
+        elapsedTime = Time.time;
+    }
+
+    void WaveSea()
+    {
+        randomNumber = Random.Range(-4, 4);
+        if (randomNumber < 0)
+        {
+            dir = -1;
+            SeaLR.gameObject.SetActive(false);
+            SeaRL.gameObject.SetActive(true);
+        }
+        else
+        {
+            dir = 1;
+            SeaLR.gameObject.SetActive(true);
+            SeaRL.gameObject.SetActive(false);
+        }
+
+    }
+}
---
+++ Script/World 5 -- Water/5-4 -- Foul Winds and Stormy Seas/Waves.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waves : MonoBehaviour {
+
+    public float waveNumber;
+    private int activated;
+    private float t;
+    private float currentTime;
+    private float direction;
+    private float checkDir;
+    
+	void Start ()
+    {
+        activated = 0;
+        t = 0;
+        currentTime = Time.time;
+        direction = SeaController.dir;
+	}
+	
+	void Update ()
+    {
+        checkDir = SeaController.dir;
+        makeWaves();
+        if (Time.time >= currentTime + waveNumber * 0.05 && t == 0)
+        {
+            activated = 1;
+            t = Time.time;
+        }
+
+        //if (direction != checkDir) gameObject.SetActive(false);
+        
+	}
+
+    void makeWaves()
+    {
+        if (activated == 1)
+        {
+            transform.position = new Vector3(transform.position.x, (Mathf.Sin((Time.time - t) / 2f) * 4f) - 8f, transform.position.z);
+        }
+    }
+}
