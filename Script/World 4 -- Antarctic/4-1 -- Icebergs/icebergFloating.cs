e92dc3181be7e97d29c9f4fe5d7f05f49f418476
typoattack <typoattack@gmail.com>
2018-08-14 20:17:53 -0400

    Rename iceberg level and add disintegrating floor level
    unnecessary duplicate files should be removed

---
+++ Script/World 4 -- Antarctic/4-1 -- Icebergs/icebergFloating.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class icebergFloating : MonoBehaviour {
+
+    public float range;
+    public float MovingSpeed;
+    bool direction;//true = left; false = right
+    Rigidbody rigid;
+    float originX;
+
+    private void Start()
+    {
+        direction = Random.Range(0f, 1f) < 0.5 ? true : false;
+        rigid = gameObject.GetComponent<Rigidbody>();
+        originX = this.transform.position.x;
+    }
+
+    void FixedUpdate () {
+		if (direction)
+        {
+            rigid.transform.Translate(-MovingSpeed * 0.01f, 0f, 0f);
+            if (gameObject.transform.position.x - originX <= -range)
+            {
+                direction = !direction;
+            }
+        }
+        else
+        {
+            rigid.transform.Translate(MovingSpeed * 0.01f, 0f, 0f);
+            if (gameObject.transform.position.x - originX >= range)
+            {
+                direction = !direction;
+            }
+        }
+	}
+}
---
+++ Script/World 4 -- Antarctic/4-3 -- Watch Your Step/FloeDestroy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloeDestroy : MonoBehaviour {
+
+    public float hp;
+    private float startHP;
+    private GameObject iceSplatter;
+    
+	void Start ()
+    {
+        startHP = hp;
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Bullet" || other.gameObject.tag == "Missile")
+        {
+            hp--;
+            if (hp < startHP) gameObject.transform.GetChild(1).gameObject.SetActive(true);
+            if (hp <= startHP * 0.5f) gameObject.transform.GetChild(2).gameObject.SetActive(true);
+            if (hp <= 0)
+            {
+                iceSplatter = GameObject.Find("FX_IceSplatter");
+                GameObject newSplatters = Instantiate(iceSplatter, transform.position, new Quaternion()) as GameObject;
+                ParticleSystem SplattersParticle = newSplatters.GetComponent<ParticleSystem>();
+                var main = SplattersParticle.main;
+                main.startSize = 0.5f;
+                main.startSpeed = 5f;
+                Destroy(newSplatters, 1.5f);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
